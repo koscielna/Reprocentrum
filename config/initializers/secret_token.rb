c32210dfb751b498b9ec3551f56e9a4bbe593a16
Aleksandra Nabożny <aleksandra.nabozny@gmail.com>
2013-10-10 21:58:43 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Reprocentrum::Application.config.secret_key_base = 'ed865a63658968c19e18ed8fda87510957a3a7e728d1a5200a055b0d8acbbf985fef0d23cf4c882353af531368323b0332d7b2dcc5f83146c6b28f324095d085'
