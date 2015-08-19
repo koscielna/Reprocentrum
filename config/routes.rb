4421226dbd2204fa50e1a7eab4758de69cb150d9
Aleksandra Nabożny <aleksandra.nabozny@gmail.com>
2015-08-19 15:01:46 +0200

    remove unnecessary

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,6 @@ Rails.application.routes.draw do
 
   get '/uslugi' => 'services#index', as: :services
   get '/kontakt' => 'contact#index', as: :contact
-  get '/doswiadczenie' => 'experience#index', as: :experience
-  get '/kadra' => 'crew#index', as: :crew
-  get '/druk' => 'print#index', as: :print
 
   post '/deliver' => 'contact#deliver'
 end
