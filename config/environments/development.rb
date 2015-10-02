0a509066479720cb370985bef3037bdbcc67351e
Aleksandra Nabożny <aleksandra.nabozny@gmail.com>
2015-10-02 20:24:18 +0200

    change smtp port number

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
       address:              ENV['SMTP_ADDRESS'],
-      port:                 587,
+      port:                 465,
       domain:               ENV['SMTP_DOMAIN'],
       user_name:            ENV['SMTP_USER_NAME'],
       password:             ENV['SMTP_PASSWORD'],
